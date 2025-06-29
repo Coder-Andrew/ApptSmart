90acd366c5834e5b73b15c0bcf740e1a37083ace
Coder-Andrew <andrewjesch@gmail.com>
2025-06-29 16:29:43 -0700

    Test trailing slash

--- apptsmart-frontend/next.config.ts
+++ apptsmart-frontend/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/app1",
-  assetPrefix: "/app1"
+  assetPrefix: "/app1",
+  trailingSlash: true
 };
 
 export default nextConfig;
